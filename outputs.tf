51e1d6a0d756918046e260ba10b4b3b544168487
Bernd Zuther <bernd.zuther@codecentric.de>
2016-04-02 14:38:47 +0200

    add internal router to output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "vpn" {
   value = "https://${aws_elb.vpn.dns_name}"
 }
 
+output "internal_master" {
+  value = "http://${aws_elb.internal_master.dns_name}"
+}
+
 output "public_slave" {
   value = "${aws_elb.public_slaves.dns_name}"
 }
