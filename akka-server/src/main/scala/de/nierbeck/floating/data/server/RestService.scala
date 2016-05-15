494ddb9ba230c8e9fe5b7be3aa1f0bc5aa23447b
anierbeck <bcanhome@googlemail.com>
2016-05-15 20:48:11 +0200

    json4s persist fails because of unkown conflict with akka

--- akka-server/src/main/scala/de/nierbeck/floating/data/server/RestService.scala
+++ akka-server/src/main/scala/de/nierbeck/floating/data/server/RestService.scala
@@ -3,21 +3,20 @@
  */
 package de.nierbeck.floating.data.server
 
-import com.datastax.driver.core.{ResultSet, Row, Session}
-import de.nierbeck.floating.data.domain.{BoundingBox, LonLat, Vehicle}
+import com.datastax.driver.core.{ ResultSet, Row, Session }
+import de.nierbeck.floating.data.domain.{ BoundingBox, LonLat, Vehicle }
 import akka.actor.ActorSystem
-import akka.http.scaladsl.server.Directives
-import akka.http.scaladsl.server.Directives._
 import akka.stream.Materializer
+import com.fasterxml.jackson.annotation.JsonValue
 import com.google.common.base.Strings
-import com.google.common.util.concurrent.{FutureCallback, Futures, ListenableFuture}
+import com.google.common.util.concurrent.{ FutureCallback, Futures, ListenableFuture }
 import de.heikoseeberger.akkahttpcirce.CirceSupport
 import de.heikoseeberger.akkahttpjson4s.Json4sSupport
 import de.nierbeck.floating.data.tiler.TileCalc
 
 import scala.collection.JavaConverters._
-import scala.concurrent.{ExecutionContext, Future, Promise}
-import scala.util.{Failure, Success, Try}
+import scala.concurrent.{ ExecutionContext, Future, Promise }
+import scala.util.{ Failure, Success, Try }
 
 trait RestService {
 
@@ -26,8 +25,10 @@ trait RestService {
   def route(session: Session)(implicit system: ActorSystem, ec: ExecutionContext) = {
 
     import org.json4s._
-    import org.json4s.JsonDSL._
     import org.json4s.jackson.JsonMethods._
+
+    import akka.http.scaladsl.server.Directives
+    import akka.http.scaladsl.server.Directives._
 
     //prepared statements for cassandra calls
     val selectTrajectoriesByBBox = session.prepare("SELECT * FROM streaming.vehicles_by_tileid WHERE tile_id = ? AND time_id = ? ")
@@ -50,20 +51,18 @@ trait RestService {
 
             val timeStamp = new java.util.Date(System.currentTimeMillis() - 3600000)
 
-            val futureResults : Set[Future[ResultSet]] = tileIds.map(tileId => session.executeAsync(selectTrajectoriesByBBox.bind(tileId, timeStamp)).toFuture)
+            val futureResults: Set[Future[ResultSet]] = tileIds.map(tileId => session.executeAsync(selectTrajectoriesByBBox.bind(tileId, timeStamp)).toFuture)
 
-            val futures: Set[Future[List[Vehicle]]] = futureResults.map(resultFuture => resultFuture.map(resultSet => resultSet.iterator().asScala.map( row => {
+            val futures: Set[Future[List[Vehicle]]] = futureResults.map(resultFuture => resultFuture.map(resultSet => resultSet.iterator().asScala.map(row => {
               Vehicle(row.getString("id"), Some(row.getTimestamp("time")), row.getDouble("latitude"), row.getDouble("longitude"), row.getInt("heading"))
             }).toList))
 
-            val futureVehicles: Future[List[Vehicle]] = Future.sequence(futures.map(futureToFutureTry(_))).map(_.collect{ case Success(x) => x}).map(set => set.toList.flatten)
+            val futureVehicles: Future[List[Vehicle]] = Future.sequence(futures.map(futureToFutureTry(_))).map(_.collect { case Success(x) => x }).map(set => set.toList.flatten)
 
-            val result:Future[String] = futureVehicles.map( vehicles => {
-              val vehicleJson =
-                ("vehicles" ->
-                  vehicles.map { v =>
-                    (("id" -> v.id))})
-              val json:String = compact(render(vehicleJson))
+            val result: Future[String] = futureVehicles.map(vehicles => {
+              val vehicleJson = transformVehicleToJson(vehicles)
+
+              val json: String = compact(render(vehicleJson))
               json
             })
 
@@ -77,7 +76,7 @@ trait RestService {
   }
 
   def futureToFutureTry[T](f: Future[T])(implicit ec: ExecutionContext): Future[Try[T]] =
-    f.map(Success(_)).recover{ case exception: Exception  => Failure(exception)}
+    f.map(Success(_)).recover { case exception: Exception => Failure(exception) }
 
   implicit class RichListenableFuture[T](lf: ListenableFuture[T]) {
     def toFuture: Future[T] = {
@@ -89,5 +88,20 @@ trait RestService {
       })
       p.future
     }
+  }
+
+  def transformVehicleToJson(vehicles: List[Vehicle]) = {
+    import org.json4s._
+    import org.json4s.JsonDSL.WithDouble._
+
+    ("vehicles" ->
+      vehicles.map { v =>
+        ("vehicle" ->
+          ("vehicle_id" -> "blubber")
+        /*("vehicle_id" -> v.id) ~
+          ("lat" -> v.latitude.toString) ~
+          ("lon" -> v.longitude.toString)*/
+        )
+      })
   }
 }
