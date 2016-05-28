91846febe5a9aa85bc9901de2d4f09b4157a29f4
anierbeck <bcanhome@googlemail.com>
2016-05-29 01:01:53 +0200

    removed sealed from trait

--- akka-server/src/main/scala/de/nierbeck/floating/data/server/actors/CassandraQuery.scala
+++ akka-server/src/main/scala/de/nierbeck/floating/data/server/actors/CassandraQuery.scala
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2016 Achim Nierbeck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.nierbeck.floating.data.server.actors
 import akka.actor.{Actor, ActorLogging}
 import com.datastax.driver.core.Session
 import de.nierbeck.floating.data.server.CassandraConnector
 
 
-sealed trait CassandraQuery extends Actor with ActorLogging {
+trait CassandraQuery extends Actor with ActorLogging {
 
   val session: Session = CassandraConnector.connect()
 
